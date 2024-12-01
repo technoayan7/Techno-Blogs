10287e4b4679e7b4e3def92c626c36dadf17d384
technoayan <ayanahmad7052@gmail.com>
2024-12-01 23:38:17 +0530

    Added Blogs

--- Components/BlogShare.js
+++ Components/BlogShare.js
@@ -1,28 +1,25 @@
 import { FaTwitter } from "react-icons/fa";
 
 function BlogShare({ data }) {
+  const formattedUrl = `https://technoblogs.onrender.com/blogs/${String(
+    data.Title.split(" ").join("-").toLowerCase()
+  )}`;
+  const tweetUrl = `https://twitter.com/intent/tweet?text=${data.Title
+    } by @technoayan&url=${encodeURIComponent(formattedUrl)}&hashtags=${data.Tags.replace(
+      /\s+/g,
+      ","
+    )}`;
+
   return (
-    <>
-      <div className="text-center pb-4">
-        <button className="bg-indigo-500 px-3 py-1 font-semibold text-white inline-flex items-center space-x-2 rounded">
-          <FaTwitter />
-          <a
-            className="twitter-share-button"
-            rel="noopener noreferrer"
-            target="_blank"
-            href={`https://twitter.com/intent/tweet?text=${
-              data.Title
-            } by @soumyajit4419
-            &url=blogs.soumya-jit.tech/blogs/${String(
-              data.Title.split(" ").join("-").toLowerCase()
-            )}
-            &hashtags=${data.Tags.split(" ")}`}
-          >
-            Tweet
-          </a>
-        </button>
-      </div>
-    </>
+    <div className="text-center pb-4">
+      <button
+        className="bg-indigo-500 px-3 py-1 font-semibold text-white inline-flex items-center space-x-2 rounded"
+        onClick={() => window.open(tweetUrl, "_blank", "noopener,noreferrer")}
+      >
+        <FaTwitter />
+        <span>Tweet</span>
+      </button>
+    </div>
   );
 }
 
--- Firebase/Firebase-admin.js
+++ Firebase/Firebase-admin.js
@@ -3,11 +3,11 @@ var admin = require("firebase-admin");
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      project_id: process.env.FIREBASE_PROJECT_ID,
+      project_id: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
       private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
       client_email: process.env.FIREBASE_CLIENT_EMAIL,
     }),
-    databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
   });
 }
 
