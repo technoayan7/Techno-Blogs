bc71095b54c411cd68befbad335dbf525bc80a49
Ayan Ahmad <87315325+technoayan7@users.noreply.github.com>
2025-06-16 04:04:29 +0530

    Update like-blog.js

--- pages/api/like-blog.js
+++ pages/api/like-blog.js
@@ -3,31 +3,47 @@ const requestIp = require("request-ip");
 const bcrypt = require("bcryptjs");
 
 export default async (req, res) => {
-  if (req.method == "POST") {
-    const clientIp = requestIp.getClientIp(req);
-    const pid = req.body.id;
+  if (req.method === "POST") {
+    try {
+      const clientIp = requestIp.getClientIp(req);
+      const pid = req.body.id;
 
-    const likeRef = db.collection("posts").doc(pid).collection("likes");
-
-    const snapshot = await likeRef.get();
-    let isFound = false;
-    let docId;
-    snapshot.forEach((doc) => {
-      if (bcrypt.compareSync(clientIp, doc.data().userIp)) {
-        isFound = true;
-        docId = doc.id;
+      if (!pid) {
+        return res.status(400).json({ error: "Blog ID is required" });
       }
-    });
 
-    if (!isFound) {
-      const salt = bcrypt.genSaltSync(10);
-      const hash = bcrypt.hashSync(clientIp, salt);
-      likeRef.add({ userIp: hash });
-    } else {
-      const res = await likeRef.doc(docId).delete();
+      const likeRef = db.collection("posts").doc(pid).collection("likes");
+
+      const snapshot = await likeRef.get();
+      let isFound = false;
+      let docId;
+      
+      snapshot.forEach((doc) => {
+        if (bcrypt.compareSync(clientIp, doc.data().userIp)) {
+          isFound = true;
+          docId = doc.id;
+        }
+      });
+
+      if (!isFound) {
+        // Add like
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(clientIp, salt);
+        await likeRef.add({ 
+          userIp: hash,
+          timestamp: db.FieldValue.serverTimestamp()
+        });
+      } else {
+        // Remove like
+        await likeRef.doc(docId).delete();
+      }
+      
+      res.status(200).json({ message: "Successful" });
+    } catch (error) {
+      console.error("Error handling like:", error);
+      res.status(500).json({ error: "Internal server error" });
     }
-    res.status(200).json({ message: "Successful" });
   } else {
-    res.status(304).json({ message: "Invalid Request" });
+    res.status(405).json({ error: "Method not allowed" });
   }
 };
