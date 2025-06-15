77428aa38994abf8cf641cec4da6f05b0aa331b4
Ayan Ahmad <87315325+technoayan7@users.noreply.github.com>
2025-06-16 04:06:56 +0530

    Update Firebase-admin.js

--- Firebase/Firebase-admin.js
+++ Firebase/Firebase-admin.js
@@ -1,14 +1,19 @@
-var admin = require("firebase-admin");
+import admin from "firebase-admin";
 
 if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      project_id: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-      client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    }),
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-  });
+  try {
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      }),
+      databaseURL: process.env.FIREBASE_DATABASE_URL,
+    });
+  } catch (error) {
+    console.log('Firebase admin initialization error', error.stack);
+  }
 }
 
-module.exports = admin.firestore();
+const db = admin.firestore();
+export default db;
