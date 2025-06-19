3b5a8e2e15120ddffbc5be4c0bf496697327e21f
Ayan Ahmad <87315325+technoayan7@users.noreply.github.com>
2025-06-20 03:24:46 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,15 @@
 import { ThemeProvider } from "next-themes";
 import { Provider } from "react-redux";
 import { store } from "../Redux/store";
-import '../styles/globals.css'
-
+import '../styles/globals.css';
+import ChatBot from "../Components/ChatBot";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <ThemeProvider attribute="class">
         <Component {...pageProps} />
+        <ChatBot />
       </ThemeProvider>
     </Provider>
   );
