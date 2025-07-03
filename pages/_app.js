245618ce404162b72e9eb180aedb758615718d6c
Ayan Ahmad <87315325+technoayan7@users.noreply.github.com>
2025-07-03 22:46:25 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,25 @@
 import { ThemeProvider } from "next-themes";
 import { Provider } from "react-redux";
 import { store } from "../Redux/store";
+import { useEffect } from "react";
 import '../styles/globals.css';
 import ChatBot from "../Components/ChatBot";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    // Disable image lazy loading for critical images
+    if (typeof window !== 'undefined') {
+      // Force immediate image loading for better UX
+      const style = document.createElement('style');
+      style.textContent = `
+        img[loading="eager"] {
+          content-visibility: auto;
+        }
+      `;
+      document.head.appendChild(style);
+    }
+  }, []);
+
   return (
     <Provider store={store}>
       <ThemeProvider attribute="class">
